caf721de2aa9ce5669d677c60a30dc4fc66de19f
BrunoDelani <br.delani@gmail.com>
2022-07-28 18:08:30 -0400

    :recycle: refactor: employee schema

--- src/app/schema/employee-schema.ts
+++ src/app/schema/employee-schema.ts
@@ -4,10 +4,10 @@ import EmployeeInterface from '../interface/employee-interface';
 const EmployeeSchema = new Schema({
   id: { type: String },
   name: { type: String, required: true },
-  cpf: { type: String, required: true },
-  office: { type: Number, required: true },
-  birthday: { type: String, required: true },
-  situation: { type: String }
+  cpf: { type: String, unique: true, required: true },
+  office: { type: String, enum: ['gerente', 'vendedor', 'caixa'], required: true },
+  birthday: { type: Date, required: true },
+  situation: { type: String, default: 'active' }
 });
 
 export default model<EmployeeInterface>('employee', EmployeeSchema);

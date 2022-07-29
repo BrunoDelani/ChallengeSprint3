628b8e72a315d8560e066e14124490e217f52ed3
BrunoDelani <br.delani@gmail.com>
2022-07-29 19:43:01 -0400

    feat: paginate find employees

--- src/app/controller/employee-controller.ts
+++ src/app/controller/employee-controller.ts
@@ -5,8 +5,36 @@ const { ObjectId } = require('mongodb');
 class EmployeeController {
   public async findEmployee (req: Request, res: Response): Promise<Response> {
     try {
-      const result = await Employee.find(req.query);
-      return res.status(200).json(result);
+      let page: number = 1;
+      if (req.query.page) {
+        page = parseInt(req.query.page.toString());
+      }
+      let limit: number = 5;
+      if (req.query.limit) {
+        limit = parseInt(req.query.limit.toString());
+      }
+      const skip: number = limit * (page - 1);
+      const employees = await Employee.find(req.query).skip(skip).limit(limit).sort('-createdOn');
+      const totalCount: number = await Employee.countDocuments(req.query);
+      const totalPages: number = Math.round(totalCount / limit);
+      if (totalCount === 0) {
+        return res.status(404).json({
+          message: 'Bad Request',
+          details: [
+            {
+              message: 'Employees not found, empty page.'
+            }
+          ]
+        });
+      }
+      return res.status(200).json({
+        employees,
+        currentPage: page,
+        pageSize: employees.length,
+        totalCount,
+        totalPages
+      }
+      );
     } catch (error) {
       return res.status(500).json({ error });
     }

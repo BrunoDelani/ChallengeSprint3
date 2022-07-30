0b26fa1f700fa5958b99b03ef3eba19315be968a
BrunoDelani <br.delani@gmail.com>
2022-07-30 14:37:57 -0400

    feat: dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import 'dotenv/config';
 
-app.listen((process.env.PORT || 3000), () => {
+app.listen((process.env.SERVER_PORT || 3000), () => {
   console.log(`Server start running ${new Date()}`);
 });
